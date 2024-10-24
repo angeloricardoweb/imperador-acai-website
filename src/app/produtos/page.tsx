272d34fb2f0d9a0df3eda36e73ecc99467c63e21
Angelo Ricardo <angeloengcomp@gmail.com>
2024-10-23 23:34:25 -0300

    Refactor social commitment section in quem-somos/page.tsx

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,3 +1,4 @@
+import Subheader from '@/components/Header/Subheader'
 import SectionCall from '@/components/Sections/SectionCall'
 import SectionProducts from '@/components/Sections/SectionProducts'
 import React from 'react'
@@ -5,6 +6,7 @@ import React from 'react'
 export default function Page() {
   return (
     <main>
+      <Subheader title={'Produtos'} />
       <SectionProducts />
       <SectionCall />
     </main>
